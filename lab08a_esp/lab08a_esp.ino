c678b48ad4b7f3288039f1029f93f7ce7b575a31
Xiqing Wang <xiqwang@RLE-EECS-MTL-DHCP-20-133.MIT.EDU>
2019-04-02 20:00:56 -0400

    adding in lab08a ESP32 code file

---
+++ lab08a_esp/lab08a_esp.ino
@@ -0,0 +1,153 @@
+#include <WiFi.h> //Connect to WiFi Network
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <mpu9255_esp32.h>
+#include<math.h>
+#include<string.h>
+
+TFT_eSPI tft = TFT_eSPI();
+const int SCREEN_HEIGHT = 160;
+const int SCREEN_WIDTH = 128;
+const int BUTTON_PIN = 5;
+const int LOOP_PERIOD = 40;
+
+MPU9255 imu; //imu object called, appropriately, imu
+
+char network[] = "6s08";  //SSID for 6.08 Lab
+char password[] = "iesc6s08"; //Password for 6.08 Lab
+char host[] = "608dev.net";
+
+//Some constants and some resources:
+const int RESPONSE_TIMEOUT = 6000; //ms to wait for response from host
+const uint16_t OUT_BUFFER_SIZE = 1000; //size of buffer to hold HTTP response
+char response[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request
+
+unsigned long primary_timer;
+
+int old_val;
+
+const int button_pin = 15;
+unsigned long timer;
+
+uint8_t old_reading; //for remember button value and detecting edges
+
+void setup() {
+  Serial.begin(115200); //for debugging if needed.
+  WiFi.begin(network, password); //attempt to connect to wifi
+  uint8_t count = 0; //count used for Wifi check times
+  Serial.print("Attempting to connect to ");
+  Serial.println(network);
+  while (WiFi.status() != WL_CONNECTED && count < 12) {
+    delay(500);
+    Serial.print(".");
+    count++;
+  }
+  delay(2000);
+  if (WiFi.isConnected()) { //if we connected then print our IP, Mac, and SSID we're on
+    Serial.println("CONNECTED!");
+    Serial.println(WiFi.localIP().toString() + " (" + WiFi.macAddress() + ") (" + WiFi.SSID() + ")");
+    delay(500);
+  } else { //if we failed to connect just Try again.
+    Serial.println("Failed to Connect :/  Going to restart");
+    Serial.println(WiFi.status());
+    ESP.restart(); // restart the ESP (proper way)
+  }
+  if (imu.setupIMU(1)) {
+    Serial.println("IMU Connected!");
+  } else {
+    Serial.println("IMU Not Connected :/");
+    Serial.println("Restarting");
+    ESP.restart(); // restart the ESP (proper way)
+  }
+  tft.init();
+  tft.setRotation(2);
+  tft.setTextSize(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_RED, TFT_BLACK); //set color of font to green foreground, black background
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  primary_timer = millis();
+}
+
+
+void loop() {
+  int reading = digitalRead(BUTTON_PIN);
+  if (reading != old_reading) {
+    if (!reading) { //just pushed!!
+      char* thing ="CHANGE ME, SEYMOUR"; //I need to be changed.
+      char request[500];
+      sprintf(request,"POST /sandbox/sc/CHANGE_ME HTTP/1.1\r\n");
+      sprintf(request+strlen(request),"Host: %s\r\n",host);
+      strcat(request,"Content-Type: application/x-www-form-urlencoded\r\n");
+      sprintf(request+strlen(request),"Content-Length: %d\r\n\r\n",strlen(thing));
+      strcat(request,thing);
+      do_http_request(host,request,response,OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
+      Serial.println(response);
+    }
+  }
+  old_reading = reading;
+  while (millis() - timer < LOOP_PERIOD);
+  timer = millis();
+}
+
+
+
+/*----------------------------------
+  char_append Function:
+  Arguments:
+     char* buff: pointer to character array which we will append a
+     char c:
+     uint16_t buff_size: size of buffer buff
+
+  Return value:
+     boolean: True if character appended, False if not appended (indicating buffer full)
+*/
+uint8_t char_append(char* buff, char c, uint16_t buff_size) {
+  int len = strlen(buff);
+  if (len > buff_size) return false;
+  buff[len] = c;
+  buff[len + 1] = '\0';
+  return true;
+}
+
+/*----------------------------------
+   do_http_request Function:
+   Arguments:
+      char* host: null-terminated char-array containing host to connect to
+      char* request: null-terminated char-arry containing properly formatted HTTP request
+      char* response: char-array used as output for function to contain response
+      uint16_t response_size: size of response buffer (in bytes)
+      uint16_t response_timeout: duration we'll wait (in ms) for a response from server
+      uint8_t serial: used for printing debug information to terminal (true prints, false doesn't)
+   Return value:
+      void (none)
+*/
+void do_http_request(char* host, char* request, char* response, uint16_t response_size, uint16_t response_timeout, uint8_t serial) {
+  WiFiClient client; //instantiate a client object
+  if (client.connect(host, 80)) { //try to connect to host on port 80
+    if (serial) Serial.print(request);//Can do one-line if statements in C without curly braces
+    client.print(request);
+    memset(response, 0, response_size); //Null out (0 is the value of the null terminator '\0') entire buffer
+    uint32_t count = millis();
+    while (client.connected()) { //while we remain connected read out data coming back
+      client.readBytesUntil('\n', response, response_size);
+      if (serial) Serial.println(response);
+      if (strcmp(response, "\r") == 0) { //found a blank line!
+        break;
+      }
+      memset(response, 0, response_size);
+      if (millis() - count > response_timeout) break;
+    }
+    memset(response, 0, response_size);
+    count = millis();
+    while (client.available()) { //read out remaining text (body of response)
+      char_append(response, client.read(), OUT_BUFFER_SIZE);
+    }
+    if (serial) Serial.println(response);
+    client.stop();
+    if (serial) Serial.println("-----------");
+  } else {
+    if (serial) Serial.println("connection failed :/");
+    if (serial) Serial.println("wait 0.5 sec...");
+    client.stop();
+  }
+}
